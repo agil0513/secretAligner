dd3bb34999094528d3176928ede149588cc5adf0
Antonio Gil <agilcaballero4@gmail.com>
2020-12-27 19:28:08 +0100

    Punto-3 y Punto-4

--- secretAligner/src/AppBundle/Controller/DefaultController.php
+++ secretAligner/src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Todo;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,9 +14,12 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $em = $this->getDoctrine()->getEntityManager();
+        $data = $em->getRepository('AppBundle:Todo')->findAll();
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'data' => $data,
         ]);
     }
 }
